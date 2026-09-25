5d6878dd46e5bab294d8f9f7cc3b2b507921f705
brandao-tomist <arthur.brandao.tomist@gmail.com>
2026-09-25 11:38:08 -0300

    Create Flíper.c

---
+++ Lista 2 - Exercícios Condicionais/Flíper.c
@@ -0,0 +1,15 @@
+int main() {
+    int p, r;
+
+    scanf("%d %d", &p, &r);
+
+    if (p == 0) {
+        printf("C\n");
+    } else if (r == 0) {
+        printf("B\n");
+    } else {
+        printf("A\n");
+    }
+
+    return 0;
+}
