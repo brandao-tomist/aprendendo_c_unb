8194e4eb4f8806612a46fc807c9b4d98308213c1
brandao-tomist <arthur.brandao.tomist@gmail.com>
2026-09-25 12:24:22 -0300

    Create Múltiplos de 13.c

---
+++ Lista 3 - Exercícios de Repetição/Múltiplos de 13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (){
+    int x,y,i,soma=0;
+
+    scanf("%d %d",&x,&y);
+    if (x>y)
+    {
+        x=y+x;
+        y=x-y;
+        x=x-y;
+    }
+    
+    for (i = x; i <= y; i++)
+    {
+        if (i%13!=0)
+        {
+            soma+=i;
+        }
+        
+    }
+    printf("%d\n",soma);
+    return 0;
+}
