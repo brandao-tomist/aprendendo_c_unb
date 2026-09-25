53c9a9c370d17b2795061e2945b10db586d6b312
brandao-tomist <arthur.brandao.tomist@gmail.com>
2026-09-25 12:04:01 -0300

    Create Distância.c

---
+++ Lista 1 - Exercícios Sequenciais/Distância.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ const int X = 60;
+ const int Y = 90;
+ int distancia,tempo;
+ 
+ scanf("%d",&distancia);
+ tempo = 2*distancia;
+ 
+ printf("%d minutos\n",tempo);
+ 
+    return 0;
+}
