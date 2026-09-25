afc090f13b2a46848099d328d6c643d1be528e2b
brandao-tomist <arthur.brandao.tomist@gmail.com>
2026-09-25 12:09:10 -0300

    Delete Lista-0/	Lista 1 - Exercícios Sequenciais directory

--- Lista-0/	Lista 1 - Exercícios Sequenciais/Salário com Bônus.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-char nome;
-double salario, vendas, total;
-    scanf("%s %lf %lf", &nome, &salario, &vendas);
-total = salario + (vendas*(0.15));
-    printf("TOTAL = R$ %.2lf\n",total);
-    return 0;
-}
